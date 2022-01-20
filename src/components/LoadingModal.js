bb2cb20c1bb7aaa4c2917a1aba760cbc4d170929
Toma Iliev <iliev.toma@gmail.com>
2022-01-19 19:59:23 -0800

    loading component added

---
+++ src/components/LoadingModal.js
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import { Spinner } from "react-bootstrap"
+import LoadingContext from "../context/loadingContext";
+
+const LoadingModal = () => {
+    const { isLoading } = useContext(LoadingContext);
+
+    return isLoading && (
+        <div style={{ position: 'absolute', top: '0', left: '0', width: '100vw', height: '100vh', zIndex: '999', backgroundColor: 'rgba(0,0,0,0.4' }}>
+            <div className="text-center" style={{width: '100%', height: '100%', position: 'relative'}}>
+                <Spinner animation="border" role="status" variant="light" style={{display: 'block', position:'fixed', top: 'calc(50% - (58px / 2))', right: 'calc(50% - (58px / 2))'}}>
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            </div>
+        </div>
+    )
+};
+
+export default LoadingModal;
