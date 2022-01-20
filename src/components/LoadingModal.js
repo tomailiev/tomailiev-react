81faeb4050cb8698c8057e5a52f94ba601e7ea8d
Toma Iliev <iliev.toma@gmail.com>
2022-01-19 20:12:43 -0800

    body blur covers full viewport

--- src/components/LoadingModal.js
+++ src/components/LoadingModal.js
@@ -6,9 +6,9 @@ const LoadingModal = () => {
     const { isLoading } = useContext(LoadingContext);
 
     return isLoading && (
-        <div style={{ position: 'absolute', top: '0', left: '0', width: '100vw', height: '100vh', zIndex: '999', backgroundColor: 'rgba(0,0,0,0.4' }}>
-            <div className="text-center" style={{width: '100%', height: '100%', position: 'relative'}}>
-                <Spinner animation="border" role="status" variant="light" style={{display: 'block', position:'fixed', top: 'calc(50% - (58px / 2))', right: 'calc(50% - (58px / 2))'}}>
+        <div className="text-center" style={{ width: '100vw', height: '100vh', position: 'fixed', zIndex: '999', backgroundColor: 'rgba(0,0,0,0.4' }}>
+            <div style={{ position: 'absolute', top: '0', left: '0', width: '100%', height: '100%', }}>
+                <Spinner animation="border" role="status" variant="light" style={{ display: 'block', position: 'fixed', top: 'calc(50% - (58px / 2))', right: 'calc(50% - (58px / 2))' }}>
                     <span className="visually-hidden">Loading...</span>
                 </Spinner>
             </div>
