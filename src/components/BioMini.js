c1934186abfa91142a27c0e7bd4cb28eea07f864
Toma Iliev <iliev.toma@gmail.com>
2022-01-21 12:34:00 -0800

    sidebar: language commit 1:english

--- src/components/BioMini.js
+++ src/components/BioMini.js
@@ -1,11 +1,12 @@
 import { Button, Col } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import english from '../text/english.json';
 
 const BioMini = () => {
     return (
         <Col md={8} className="fs-4">
             <h3 className="my-3">Bio</h3>
-            <p>Toma Iliev is a violinist focused on historically informed performance. Holder of the Ruth and Charles Poindexter chair with the Portland Baroque Orchestra, recent engagements include performances with Philharmonia Baroque Orchestra, American Bach Soloists, Oregon Bach Festival, Indianapolis Baroque Orchestra, Clarion Music Society, and the Valley of the Moon Festival, as well as broadcasts on MediciTV, BBC3 radio, WQXR, and the CBC “Rush Hour” series.</p>
+            <p>{english.miniBio}</p>
             <LinkContainer to="/about">
                 <Button size="sm" variant="outline-dark">Continue reading &#187;</Button>
             </LinkContainer>
--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -1,17 +1,19 @@
 import { Ratio, Spinner } from "react-bootstrap";
+import { useState } from "react";
 
 const Iframe = ({ media, size }) => {
+    const [hasLoaded, setHasLoaded] = useState(false);
     return (
-        <Ratio aspectRatio={size || '16x9'}>
-            {media.audioUrl || media.videoUrl
-                ? <iframe title={media.title} src={media.audioUrl || media.videoUrl}></iframe>
-                : <div>
-                    <Spinner animation="border" role="status">
+        <>
+            <Ratio aspectRatio={size || '16x9'}>
+                <div>
+                    <iframe onLoad={() => setHasLoaded(true)} title={media.title} src={media.audioUrl || media.videoUrl} style={{width: '100%', height: '100%'}} />
+                    {!hasLoaded && <Spinner variant="dark" animation="border" role="status" style={{ zIndex: 800, position: 'absolute', top: 'calc(50% - (58px / 2))', right: 'calc(50% - (58px / 2))' }}>
                         <span className="visually-hidden">Loading...</span>
-                    </Spinner>
+                    </Spinner>}
                 </div>
-            }
-        </Ratio>
+            </Ratio>
+        </>
     );
 };
 
