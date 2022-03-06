84f4d58d43f7c050301e7e1ff72ea97932cec050
Toma Iliev <iliev.toma@gmail.com>
2022-03-05 22:09:53 -0500

    added flag + display special messages

--- src/styles-js/banner.js
+++ src/styles-js/banner.js
@@ -3,7 +3,7 @@ import backupBannerImage from '../imgs/6666.jpg';
 
 const bannerCSS = {
     height: '75vh',
-    background: `url(${bannerImage}) no-repeat center center, url(${backupBannerImage}) no-repeat center center`,
+    background: `linear-gradient(180deg, rgba(82, 101, 182, 0.75) 49%, rgba(224, 206, 134, 0.75) 51%), url(${bannerImage}) no-repeat center center, url(${backupBannerImage}) no-repeat center center`,
     backgroundSize: 'cover',
     transition: 'all 2s ease-in',
 };
